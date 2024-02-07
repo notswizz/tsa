11031828352ccfda2a681c1fcce214a505dc9f05
swizz <emailswizz@gmail.com>
2024-02-07 16:50:20 -0500

    ok

--- components/ThemeSwitcher.js
+++ components/ThemeSwitcher.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 
 const themes = [
   "light", "dark", "cupcake", "bumblebee", "emerald", "corporate",
-  "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden",
-  "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black",
-  "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade",
-  "night", "coffee", "winter", "dim", "nord", "sunset"
+  "retro", "cyberpunk", "valentine", "garden",
+  "aqua", "lofi", "pastel", "fantasy", "wireframe", "dracula", "cmyk", "autumn",  "acid", "lemonade",
+ "coffee", "winter", "dim", "sunset"
 ]; // Add more themes as needed
 
 export default function ThemeSwitcher() {
@@ -21,7 +20,7 @@ export default function ThemeSwitcher() {
   return (
     <div className="absolute top-0 right-0 m-4">
       <button onClick={toggleDropdown} className="btn btn-ghost">
-        Change Theme
+        Theme
       </button>
       {isOpen && (
         <div className="dropdown-content mt-2 p-2 shadow bg-base-100 rounded-box" style={{ width: 'auto', display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', gap: '1rem' }}>
