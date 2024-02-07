080443d4af0f09aa9df4817729b275c7cf3de82e
swizz <emailswizz@gmail.com>
2024-02-07 16:45:06 -0500

    ok

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,14 @@ export default function Home() {
         <title>The Smith Agency</title>
       </Head>
       <main className="flex flex-col items-center justify-center min-h-screen p-4 text-center">
+      <img src="/tsablack.png" alt="TSA Logo" className="rounded-full border-4 border-secondary w-24 h-24" />
         <h1 className="text-5xl font-bold mb-4">The Smith Agency</h1>
         <p className="text-lg mb-8">Premiere Boutique Staffing</p>
         
         <ThemeSwitcher />
 
         <div className="flex gap-4 mb-8">
-          <button onClick={toggleAvailabilityModal} className="btn btn-primary">Availability Form</button>
+          <button onClick={toggleAvailabilityModal} className="btn btn-primary"> Availability Form</button>
           <button onClick={toggleNewStaffFormModal} className="btn btn-secondary">Apply to TSA</button>
         </div>
 
